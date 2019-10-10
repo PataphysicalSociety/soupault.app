0b04526153924f1859e2ce649096667e78036a79
Daniil Baturin <daniil@baturin.org>
2019-10-10 07:21:21 +0700

    Initial import.

---
+++ scripts/index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+import pystache
+
+template = """
+<h2><a href="{{url}}">{{title}}</a></h2>
+<p>Last update: {{date}}</p>
+<p>{{{excerpt}}}</p>
+<a href="{{url}}">Read more</a>
+"""
+
+renderer = pystache.Renderer()
+
+input = sys.stdin.readline()
+index_entries = json.loads(input)
+
+for entry in index_entries:
+    print(renderer.render(template, entry))
