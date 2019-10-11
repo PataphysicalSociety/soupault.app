d7562bceb39ab63419e1fde05c679db05e0741aa
Daniil Baturin <daniil@baturin.org>
2019-10-12 03:05:30 +0700

    Blog stuff and minor tweaks to the navigation menu.

--- scripts/index.py
+++ scripts/index.py
@@ -16,6 +16,7 @@ renderer = pystache.Renderer()
 
 input = sys.stdin.readline()
 index_entries = json.loads(input)
+index_entries.reverse()
 
 for entry in index_entries:
     print(renderer.render(template, entry))
