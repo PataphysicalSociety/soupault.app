a4a03a8b9f02b30dd195443d1124c216961bc40b
Daniil Baturin <daniil@baturin.org>
2022-04-19 13:12:49 +0300

    Clean up the pandoc code blocks filter a bit

--- helpers/cmark-code-blocks.lua
+++ helpers/cmark-code-blocks.lua
@@ -1,21 +1,21 @@
 -- The CommonMark spec suggests that 
 -- "```somelang" should produce <code class="language-somelang">
 -- 
--- That's what cmark and pretty much every other CommonMark implementation does
+-- That's what most CommonMark implementation do.
 -- Pandoc, however, outputs <code class="somelang">
 --
--- This makes using external highlighter much more difficult
+-- This makes using external highlighters much more difficult
 -- since you cannot match all code blocks with a known language
 -- using a single CSS selector.
 --
 -- This filter takes over the code block rendering process
--- to a produce CommonMark-style output.
+-- to produce CommonMark-style output.
 
 function CodeBlock(block)
   if FORMAT:match 'html' then
     local lang_attr = ""
     if (#block.classes > 0) then
-      lang_attr = string.format("class=\"language-%s\"", block.classes[1])
+      lang_attr = string.format([[class="language-%s"]], block.classes[1])
     else
       -- Ignore code blocks where language is not specified
     end
